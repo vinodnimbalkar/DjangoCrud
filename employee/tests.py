1d7eb92ae902c183ba1d6b19ec301f6929fb0499
Vinod Nimbalkar <omvinod@yahoo.com>
2019-01-17 17:49:08 +0530

    Test cases added

--- employee/tests.py
+++ employee/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+from employee.models import Employee
+
+
+class EmployeeTestCase(TestCase):
+    '''This is Employee Test Case'''
+    def setUp(self):
+        Employee.objects.create(
+            eid="1", ename="Vinod", eemail="abc@gmail.com", econtact="0123456789"
+        )
+        Employee.objects.create(
+            eid="2", ename="Vinay", eemail="xyz@gmail.com", econtact="9876543210"
+        )
+
+    def test_employee(self):
+        """Employee detail correctly identified"""
+        vinod = Employee.objects.get(eid="1")
+        vinay = Employee.objects.get(eid="2")
